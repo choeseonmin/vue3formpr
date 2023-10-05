6d0a5d58b2ab11c522abcdc1706c83ea3d3c0efe
albamm520@naver.com <albamm520@naver.com>
2023-10-05 18:07:45 +0900

    버튼 연습 1 ~ 2

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,29 @@
   <a-button type="text">asd</a-button>
   <a-button type="link">asdasd</a-button>
   <a-button type="primary">asdas</a-button>
+<!--  disabled는 클릭 안됨-->
+  <a-button type="primary" disabled>asdasd</a-button>
+  <a-button>기본</a-button>
+  <a-button disabled>기본(disabled)</a-button>
+  <a-button type="dashed">대쉬</a-button>
+  <a-button type="dashed" disabled>대시 안됨</a-button>
+  <a-button type="text">텍스트</a-button>
+  <a-button type="text" disabled>텍스트</a-button>
+  <a-button type="link">링크</a-button>
+  <a-button type="link" disabled>링크 안됨</a-button>
+  <a-button danger>danger</a-button>
+  <a-button danger disabled>danger</a-button>
+  <a-button type="text" danger>danger 텍스트</a-button>
+  <a-button type="text" danger disabled>danger 텍스트</a-button>
+  <a-button type="link" danger>링크 danger</a-button>
+  <a-button type="link" danger disabled>링크 danger</a-button>
+  <div :style="{ padding: '8px 8px 0 8px', background: 'rgb(190, 200, 200)' }">
+    <a-button ghost>고스트</a-button>
+    <a-button ghost disabled>고스트</a-button>
+  </div>
+
+
+
 </template>
 
 <script setup>
