8a740aea645aedbfc76adf225fe86e3175fcc01e
albamm520@naver.com <albamm520@naver.com>
2023-10-05 18:23:45 +0900

    버튼 연습 3

--- src/App.vue
+++ src/App.vue
@@ -25,10 +25,49 @@
     <a-button ghost>고스트</a-button>
     <a-button ghost disabled>고스트</a-button>
   </div>
-
-
-
+  <a-space style="width: 100%">
+      <a-button type="primary" size="small" loading>작은 로딩</a-button>
+      <a-button type="primary" loading>로딩</a-button>
+  </a-space>
+  <a-space style="width: 100%">
+    <a-button type="primary" :loading="loading" @mouseenter="loading = true">마우스 엔터</a-button>
+    <a-button type="primary" :loading="iconLoading" @click="enterIconLoading">
+      <template #icon><PoweroffOutlined /></template>
+      1초
+    </a-button>
+  </a-space>
+  <a-space style="width: 100%">
+    <a-button type="primary" loading />
+    <a-button type="primary" shape="circle" loading/>
+    <a-button danger shape="round" loading/>
+  </a-space>
 </template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import { PoweroffOutlined } from '@ant-design/icons-vue';
+
+interface DelayLoading {
+  delay: number;
+}
+export default defineComponent({
+  components: { PoweroffOutlined },
+  setup() {
+    const iconLoading = ref<boolean | DelayLoading>(false);
+    const enterIconLoading = () => {
+      iconLoading.value = { delay: 1000 };
+
+      setTimeout(() => {
+        iconLoading.value = false;
+      }, 6000);
+    };
+    return {
+      loading: ref(false),
+      iconLoading,
+      enterIconLoading,
+    };
+  },
+});
+</script>
 
 <script setup>
 </script>
